acd7499b33b16c22d1e8af45ff1f313d1af2cc37
ArulRozario <33756400+ArulRozario@users.noreply.github.com>
2023-08-18 19:32:05 +0530

    map on a primeng popup

--- src/app/data-table/data-table.component.ts
+++ src/app/data-table/data-table.component.ts
@@ -5,35 +5,37 @@ import { MapComponent } from '../map/map.component';
 @Component({
   selector: 'app-data-table',
   templateUrl: './data-table.component.html',
-  styleUrls: ['./data-table.component.scss']
+  styleUrls: ['./data-table.component.scss'],
 })
 export class DataTableComponent implements OnInit {
-
   data = routesData;
   routeVisible = false;
+  mapVisible = false;
+  routeCoordinates = [];
+  @ViewChild(MapComponent) map: MapComponent;
 
-  @ViewChild(MapComponent) map:MapComponent;
-
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
   // select all is buggy. Manually checked and unchecked checkboxes don't get affected by selectAll. It's a very primitive solution for demo only. Focus is on map
   selectAll(event) {
-    const status =event.target.checked;
+    const status = event.target.checked;
     const checkboxes = document.querySelectorAll('input[type="checkbox"]');
-    checkboxes.forEach(checkbox => checkbox.removeAttribute('checked'));
+    checkboxes.forEach((checkbox) => checkbox.removeAttribute('checked'));
     if (status) {
-      checkboxes.forEach(checkbox => checkbox.setAttribute('checked', 'true'));
+      checkboxes.forEach((checkbox) =>
+        checkbox.setAttribute('checked', 'true')
+      );
     }
   }
-  route(){
-    const checkboxes = document.querySelectorAll('input[type="checkbox"]:checked');
+  route() {
+    const checkboxes = document.querySelectorAll(
+      'input[type="checkbox"]:checked'
+    );
     const coordinates = [];
-    checkboxes.forEach(checkbox=> {
-      if((checkbox as any).value === 'on') return; // for the selectAll checkbox.
+    checkboxes.forEach((checkbox) => {
+      if ((checkbox as any).value === 'on') return; // for the selectAll checkbox.
       coordinates.push((checkbox as any).value);
     });
-    this.map.getRoutes(coordinates);
+    this.routeCoordinates = coordinates;
+    this.mapVisible = true;
   }
-
-
 }
--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,51 +1,68 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, Input, OnInit } from '@angular/core';
 declare let L;
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
-  styleUrls: ['./map.component.scss']
+  styleUrls: ['./map.component.scss'],
 })
-export class MapComponent implements OnInit {
-  map:any;
-  routingControl:any;
-  constructor(private http:HttpClient) { 
-  }
+export class MapComponent implements AfterViewInit {
+  map: any;
+  routingControl: any;
+  @Input() routeCoordinates = [];
+  constructor(private http: HttpClient) {}
 
-  ngOnInit() {
-    this.renderMap();
+  ngAfterViewInit() {
+    setTimeout(() => {
+      // wait for the component to come to view then render the map. map element is available to leaflet only after the component is fully in the view.
+      this.renderMap();
+      this.renderRoutes();
+    });
   }
 
   private renderMap() {
-    if(this.map) this.map.remove();
-    this.map = L.map('map').setView([35.989, 	-80.22],4);
+    if (this.map) this.map.remove();
+    this.map = L.map('map').setView([35.989, -80.22], 4);
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
       maxZoom: 19,
-      minZoom:3,
-      attribution: '© OpenStreetMap'
+      minZoom: 3,
+      attribution: '© OpenStreetMap',
     }).addTo(this.map);
   }
-  private drawRoute(routes){
+  private drawRoute(routes) {
     // flip lat lng positions in the array for leaflet.
-    const routePoints = routes.waypoints.map(point=> [point.location[1],point.location[0]]);
+    const routePoints = routes.waypoints.map((point) => [
+      point.location[1],
+      point.location[0],
+    ]);
     this.routingControl && this.map.removeControl(this.routingControl);
     this.routingControl = L.Routing.control({
       waypoints: routePoints,
       showAlternatives: true,
-      lineOptions: { styles: [{ color: 'green', weight: 7 }], extendToWaypoints: false, missingRouteTolerance: 1 },
+      lineOptions: {
+        styles: [{ color: 'green', weight: 7 }],
+        extendToWaypoints: false,
+        missingRouteTolerance: 1,
+      },
       fitSelectedRoutes: false,
-      altLineOptions: { styles: [{ color: 'green', weight: 7 }], extendToWaypoints: false, missingRouteTolerance: 1 },
+      altLineOptions: {
+        styles: [{ color: 'green', weight: 7 }],
+        extendToWaypoints: false,
+        missingRouteTolerance: 1,
+      },
       show: false,
       routeWhileDragging: false,
-    })
+    });
     this.routingControl.addTo(this.map);
   }
-  getRoutes(coordinates?:any[]){
-    if(!coordinates?.length) return;
+  renderRoutes() {
+    if (!this.routeCoordinates?.length) return;
     const barUrl = `https://router.project-osrm.org/route/v1/driving`;
     const params = `overview=false&alternatives=false&steps=false`;
     // convert the API call to promise to close the stream immediately.
-    this.http.get(`${barUrl}/${coordinates?.join(';')}?${params}`).toPromise().then(res=> this.drawRoute(res));
+    this.http
+      .get(`${barUrl}/${this.routeCoordinates?.join(';')}?${params}`)
+      .toPromise()
+      .then((res) => this.drawRoute(res));
   }
-
 }
